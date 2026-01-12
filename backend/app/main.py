0214e9641107bc66acf0733d109b1ad717d2c6fb
Pletka Daniil <pletkadaniil@yandex.ru>
2026-01-13 01:29:34 +0300

    + database creating

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
 from app.routes.router import router
+from app.database.database import init_db
 
 app = FastAPI()
+init_db()
 
 app.include_router(
     router=router, prefix='/api'
