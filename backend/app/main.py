73f55dc6e123a0356fe8e74c042cf8a19cfe4440
Pletka Daniil <pletkadaniil@yandex.ru>
2026-04-01 17:47:49 +0300

    + new router for files

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from app.routes.router import router
+from app.routes.file_router import file_router
 from app.database.database import init_db
 from fastapi.middleware.cors import CORSMiddleware
 from app.config.config import settings
@@ -18,4 +19,7 @@ init_db()
 
 app.include_router(
     router=router, prefix='/api'
+)
+app.include_router(
+    router=file_router, prefix='/api'
 )
