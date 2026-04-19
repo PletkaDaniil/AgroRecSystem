8b7ca7e47e4405b191533ea9af0169f990a0ffde
Pletka Daniil <pletkadaniil@yandex.ru>
2026-04-19 21:09:46 +0300

    + image resolution field to ProcessRequest

--- backend/app/utils/schemas/processRequest.py
+++ backend/app/utils/schemas/processRequest.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, field_validator
 from typing import Literal
 
 
@@ -6,7 +6,7 @@ class Bands(BaseModel):
     nir: int | None = None
     red: int | None = None
     red_edge: int | None = None
-    green: int | None = None
+    blue: int | None = None
     b1: int | None = None
     b2: int | None = None
 
@@ -21,5 +21,12 @@ class ProcessRequest(BaseModel):
     # критерий сегментации
     segmentation_level: int = Field(ge=3, le=5)
 
+    # разрешение снимка в сантиметрах на пиксель
+    resolution: float = Field(..., gt=0)
+
     # каналы
     bands: Bands
+
+    @field_validator("resolution", mode="before")
+    def parse_resolution(cls, v):
+        return float(v.replace(",", ".")) if isinstance(v, str) else v
