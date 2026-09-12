5d73a9341f6a09ca3a2bf100ecd3d62a917cbdf6
Pletka Daniil <pletkadaniil@yandex.ru>
2026-09-12 20:13:31 +0300

    + redirect to the user cabinet

--- frontend/src/components/AppHeader.vue
+++ frontend/src/components/AppHeader.vue
@@ -32,7 +32,7 @@
     </div>
 
     <div class="auth">
-      <router-link to="/login" class="login-btn">
+      <router-link to="/cabinet" class="login-btn">
         <svg width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
           <path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"/>
           <circle cx="12" cy="7" r="4"/>
